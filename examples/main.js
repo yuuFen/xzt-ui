a5e2aff7e83c790a8ce6364ee2aeb6badc45dd81
Yu <me@yuufen.com>
2020-03-05 15:57:26 +0800

    create 挂载至body

--- examples/main.js
+++ examples/main.js
@@ -5,14 +5,15 @@ import XztUi from '../packages/index'
 import '../packages/index.scss'
 Vue.use(XztUi)
 
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
+Vue.use(MuseUI)
 
-import MuseUI from 'muse-ui';
-import 'muse-ui/dist/muse-ui.css';
-Vue.use(MuseUI);
-
+import create from '../src/utils/create'
+Vue.prototype.$create = create
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
 }).$mount('#app')
